70811cb71675bdef03c6095ef176a34a0257f781
Jack Ren <bjrjk@qq.com>
2020-10-13 18:04:06 +0800

    20201013 1804

--- src/Main3.java
+++ src/Main3.java
@@ -12,9 +12,10 @@ class Main3 {
 	}
 
 	static class PositionNode {
-		public int x, y, priority;
+		public int x, y;
+		public long priority;
 
-		public PositionNode(int x, int y, int priority) {
+		public PositionNode(int x, int y, long priority) {
 			this.x = x;
 			this.y = y;
 			this.priority = priority;
@@ -23,13 +24,13 @@ class Main3 {
 		public static Comparator<PositionNode> cmpGreater = new Comparator<PositionNode>() {
 			@Override
 			public int compare(PositionNode o1, PositionNode o2) {
-				return o2.priority - o1.priority;
+				return (int) (o2.priority - o1.priority);
 			}
 		};
 		public static Comparator<PositionNode> cmpLess = new Comparator<PositionNode>() {
 			@Override
 			public int compare(PositionNode o1, PositionNode o2) {
-				return o1.priority - o2.priority;
+				return (int) (o1.priority - o2.priority);
 			}
 		};
 	}
@@ -40,7 +41,8 @@ class Main3 {
 		private int[][] grid = new int[SIZE][SIZE];
 		private int[] XShift = { -1, -1, -1, 0, 0, 1, 1, 1 };
 		private int[] YShift = { -1, 0, 1, -1, 1, -1, 0, 1 };
-		private int[] Score = { 0, 10, 100, 1000, 10000, 100000 };
+		private long[] Score_E1 = { 0, 2, 40, 500, 5000, 70000, 750000, 8000000, 80000000 };
+		private long[] Score_E2 = { 0, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000 };
 
 		public void placeAt(int x, int y, int value) {
 			if (x >= 0 && y >= 0 && x < SIZE && y < SIZE)
@@ -54,157 +56,210 @@ class Main3 {
 				return NOT_EXIST;
 		}
 
-		private int getScore(int status, int cnt) {
+		private long getScore(int status, int cnt, int edgeSituation) {
 			// assert (0 <= cnt && cnt <= 5);
 			assert (status == EMPTY || status == BOT || status == PLAYER);
-			switch (status) {
-			case BOT:
-				return cnt > 5 ? Score[5] : Score[cnt];
-			case PLAYER:
-				return -(cnt > 5 ? Score[5] : Score[cnt]);
-			default:
+			if (edgeSituation == 0)
 				return 0;
+			else if (edgeSituation == 1) {
+				switch (status) {
+				case BOT:
+					return cnt >= Score_E1.length ? Score_E1[Score_E1.length - 1] : Score_E1[cnt];
+				case PLAYER:
+					return -(cnt >= Score_E1.length ? Score_E1[Score_E1.length - 1] : Score_E1[cnt]);
+				default:
+					return 0;
+				}
+			} else { // edgeSituation==2
+				switch (status) {
+				case BOT:
+					return cnt >= Score_E2.length ? Score_E2[Score_E2.length - 1] : Score_E2[cnt];
+				case PLAYER:
+					return -(cnt >= Score_E2.length ? Score_E2[Score_E2.length - 1] : Score_E2[cnt]);
+				default:
+					return 0;
+				}
 			}
 		}
 
-		private int SequenceEvaluate(ArrayList<Integer> XList, ArrayList<Integer> YList) {
-			assert (XList.size() == YList.size());
-			int sum = 0;
+		private int calculateEdgeSituation(int leftEdgeStatus, int rightEdgeStatus) {
+			int cnt = 0;
+			if (EMPTY == leftEdgeStatus)
+				cnt++;
+			if (EMPTY == rightEdgeStatus)
+				cnt++;
+			return cnt;
+		}
+
+		private int getElemInArraySafe(int[] list, int index, int size) {
+			if (0 <= index && index < size)
+				return list[index];
+			else
+				return NOT_EXIST;
+		}
+
+		private long SequenceEvaluate(int[] XList, int[] YList, int size) {
+			int XSize = size, YSize = size;
+			long sum = 0;
 			int status = EMPTY, cnt = 0;
-			for (int i = 0; i < XList.size(); i++) {
-				int curGrid = getValueAt(XList.get(i), YList.get(i));
+			int leftEdge = -1, rightEdge;
+			for (int i = 0; i < XSize; i++) {
+				int curGrid = getValueAt(XList[i], YList[i]);
 				if (status == curGrid)
 					cnt++;
 				else {
-					sum += getScore(status, cnt);
+					rightEdge = i;
+					sum += getScore(status, cnt,
+							calculateEdgeSituation(
+									getValueAt(getElemInArraySafe(XList, leftEdge, XSize),
+											getElemInArraySafe(YList, leftEdge, YSize)),
+									getValueAt(getElemInArraySafe(XList, rightEdge, XSize),
+											getElemInArraySafe(YList, rightEdge, YSize))));
 					status = curGrid;
 					cnt = 1;
+					leftEdge = i - 1;
 				}
 			}
-			sum += getScore(status, cnt);
+			rightEdge = XSize;
+			sum += getScore(status, cnt,
+					calculateEdgeSituation(
+							getValueAt(getElemInArraySafe(XList, leftEdge, XSize),
+									getElemInArraySafe(YList, leftEdge, YSize)),
+							getValueAt(getElemInArraySafe(XList, rightEdge, XSize),
+									getElemInArraySafe(YList, rightEdge, YSize))));
 			return sum;
 		}
 
-		private int Evaluate() { // 棋局评估函数
-			int sum = 0;
-			ArrayList<Integer> XList = new ArrayList<Integer>();
-			ArrayList<Integer> YList = new ArrayList<Integer>();
+		private long Evaluate() { // 棋局评估函数
+			long sum = 0;
+			int ptr = 0;
+			int[] XList = new int[50];
+			int[] YList = new int[50];
 			// 评估每一行
 			for (int i = 0; i < SIZE; i++) {
-				XList.clear();
-				YList.clear();
+				ptr = 0;
 				for (int j = 0; j < SIZE; j++) {
-					XList.add(i);
-					YList.add(j);
+					XList[ptr] = i;
+					YList[ptr] = j;
+					ptr++;
 				}
-				sum += SequenceEvaluate(XList, YList);
+				sum += SequenceEvaluate(XList, YList, ptr);
 			}
 			// 评估每一列
 			for (int i = 0; i < SIZE; i++) {
-				XList.clear();
-				YList.clear();
+				ptr = 0;
 				for (int j = 0; j < SIZE; j++) {
-					XList.add(j);
-					YList.add(i);
+					XList[ptr] = j;
+					YList[ptr] = i;
+					ptr++;
 				}
-				sum += SequenceEvaluate(XList, YList);
+				sum += SequenceEvaluate(XList, YList, ptr);
 			}
 			// 评估每个左上-右下对角线
 
 			// 左侧第0列开始的对角线
 			for (int line = 0; line < SIZE; line++) {
-				XList.clear();
-				YList.clear();
+				ptr = 0;
 				for (int x = line, y = 0; x < SIZE && y < SIZE; x++, y++) {
-					XList.add(x);
-					YList.add(y);
+					XList[ptr] = x;
+					YList[ptr] = y;
+					ptr++;
 				}
-				sum += SequenceEvaluate(XList, YList);
+				sum += SequenceEvaluate(XList, YList, ptr);
 			}
 
 			// 上侧第0行开始的对角线
 			for (int row = 1; row < SIZE; row++) {
-				XList.clear();
-				YList.clear();
+				ptr = 0;
 				for (int x = 0, y = row; x < SIZE && y < SIZE; x++, y++) {
-					XList.add(x);
-					YList.add(y);
+					XList[ptr] = x;
+					YList[ptr] = y;
+					ptr++;
 				}
-				sum += SequenceEvaluate(XList, YList);
+				sum += SequenceEvaluate(XList, YList, ptr);
 			}
 			// 评估每个右上-左下对角线
 
 			// 右侧第14列开始的对角线
 			for (int line = 0; line < SIZE; line++) {
-				XList.clear();
-				YList.clear();
+				ptr = 0;
 				for (int x = line, y = SIZE - 1; x < SIZE && y >= 0; x++, y--) {
-					XList.add(x);
-					YList.add(y);
+					XList[ptr] = x;
+					YList[ptr] = y;
+					ptr++;
 				}
-				sum += SequenceEvaluate(XList, YList);
+				sum += SequenceEvaluate(XList, YList, ptr);
 			}
 
 			// 上侧第0行开始的对角线
 			for (int row = 0; row < SIZE - 1; row++) {
-				XList.clear();
-				YList.clear();
+				ptr = 0;
 				for (int x = 0, y = row; x < SIZE && y >= 0; x++, y--) {
-					XList.add(x);
-					YList.add(y);
+					XList[ptr] = x;
+					YList[ptr] = y;
+					ptr++;
 				}
-				sum += SequenceEvaluate(XList, YList);
+				sum += SequenceEvaluate(XList, YList, ptr);
 			}
 			return sum;
 		}
 
-		private int EvaluateUnit(int x, int y) {
-			int sum = 0;
-			ArrayList<Integer> XList = new ArrayList<Integer>();
-			ArrayList<Integer> YList = new ArrayList<Integer>();
+		private long EvaluateUnit(int x, int y) {
+			long sum = 0;
+			int ptr = 0;
+			int[] XList = new int[50];
+			int[] YList = new int[50];
 			// 评估行
+			ptr = 0;
 			for (int j = 0; j < SIZE; j++) {
-				XList.add(x);
-				YList.add(j);
+				XList[ptr] = x;
+				YList[ptr] = j;
+				ptr++;
 			}
-			sum += SequenceEvaluate(XList, YList);
+			sum += SequenceEvaluate(XList, YList, ptr);
 			// 评估列
+			ptr = 0;
 			for (int j = 0; j < SIZE; j++) {
-				XList.add(j);
-				YList.add(y);
+				XList[ptr] = j;
+				YList[ptr] = y;
+				ptr++;
 			}
-			sum += SequenceEvaluate(XList, YList);
+			sum += SequenceEvaluate(XList, YList, ptr);
 			// 评估左上-右下对角线
+			ptr = 0;
 			for (int i = x - Math.min(x, y), j = y - Math.min(x, y); i < SIZE && j < SIZE; i++, j++) {
-				XList.add(i);
-				YList.add(j);
+				XList[ptr] = i;
+				YList[ptr] = j;
+				ptr++;
 			}
-			sum += SequenceEvaluate(XList, YList);
+			sum += SequenceEvaluate(XList, YList, ptr);
 
 			// 评估右上-左下对角线
+			ptr = 0;
 			for (int i = x - Math.min(x, SIZE - y), j = y + Math.min(x, SIZE - y); i < SIZE && j >= 0; i++, j--) {
-				XList.add(i);
-				YList.add(j);
+				XList[ptr] = i;
+				YList[ptr] = j;
+				ptr++;
 			}
-			sum += SequenceEvaluate(XList, YList);
+			sum += SequenceEvaluate(XList, YList, ptr);
 			return sum;
 		}
 
-		private int EvaluateUnitDiff(int depth, int x, int y) {
+		private long EvaluateUnitDiff(int depth, int x, int y) {
 			placeAt(x, y, EMPTY);
-			int sum1 = EvaluateUnit(x, y);
+			long sum1 = EvaluateUnit(x, y);
 			placeAt(x, y, depth % 2 == 0 ? BOT : PLAYER);
-			int sum2 = EvaluateUnit(x, y);
+			long sum2 = EvaluateUnit(x, y);
 			placeAt(x, y, EMPTY);
 			return sum2 - sum1;
 		}
 
-		private int DFS2(int depth, Position movePos, int cutValue, int evaluationValue) {
+		private long DFS2(int depth, Position movePos, long cutValue, long evaluationValue) {
 			// 极大极小搜索, depth%2==0时为BOT,depth%2==1时为PLAYER
 			// alpha-beta 剪枝, cutValue
 			if (depth == DEPTH)
 				return evaluationValue;
-			int selectedScore = depth % 2 == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+			long selectedScore = depth % 2 == 0 ? Long.MIN_VALUE : Long.MAX_VALUE;
 			Queue<PositionNode> pq = new PriorityQueue<PositionNode>(
 					depth % 2 == 0 ? PositionNode.cmpGreater : PositionNode.cmpLess);
 			for (int i = 0; i < SIZE; i++) {
@@ -223,15 +278,12 @@ class Main3 {
 						pq.add(new PositionNode(i, j, EvaluateUnitDiff(depth, i, j)));
 				}
 			}
-			int nodeCnt = 0;
 			while (!pq.isEmpty()) {
-				nodeCnt++;
-				if (nodeCnt > 100)
-					break;
 				PositionNode curPositionNode = pq.poll();
 				int i = curPositionNode.x, j = curPositionNode.y;
 				placeAt(i, j, depth % 2 == 0 ? BOT : PLAYER);
-				int curScore = DFS2(depth + 1, new Position(), selectedScore, evaluationValue+curPositionNode.priority);
+				long curScore = DFS2(depth + 1, new Position(), selectedScore,
+						evaluationValue + curPositionNode.priority);
 				if (depth % 2 == 0) {
 					if (selectedScore < curScore) {
 						selectedScore = curScore;
@@ -248,10 +300,10 @@ class Main3 {
 				placeAt(i, j, EMPTY);
 				// alpha-beta剪枝
 				if (depth % 2 == 0) {
-					if (selectedScore > cutValue) // beta剪枝
+					if (selectedScore >= cutValue) // beta剪枝
 						break;
 				} else {
-					if (selectedScore < cutValue) // alpha剪枝
+					if (selectedScore <= cutValue) // alpha剪枝
 						break;
 				}
 			}
@@ -260,7 +312,7 @@ class Main3 {
 
 		public Map<String, Integer> ChoosePosition() { // 选择位置
 			Position move = new Position();
-			DFS2(0, move, Integer.MAX_VALUE,0);
+			DFS2(0, move, Long.MAX_VALUE, Evaluate());
 			Map<String, Integer> map = new HashMap<String, Integer>();
 			map.put("x", move.x);
 			map.put("y", move.y);
